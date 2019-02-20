5978cb4f1ff20db078871cb1836479a6f679182b
David Williams <dwilliams@icstars.org>
2019-02-20 15:14:41 -0600

    Changed the HTML for the site.

---
+++ mysite/blog/urls.py
@@ -0,0 +1,9 @@
+#Import django function path and all of our views
+#from the blog app.
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #assign post_list view to root url
+    path('',views.post_list,name='post_list'),
+]
